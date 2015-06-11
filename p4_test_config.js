bde6723a2971ed5556929b781a539765f71050cd
Ashley Davis <ashley.davis@realseriousgames.com>
2015-06-11 14:43:37 +1000

    Removed details from test config file.

--- p4_test_config.js
+++ p4_test_config.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-	p4User: 'ash',
-	p4Workspace: 'ash_1769_GEN',
-    p4Host: 'seriousserver:1666',
+	p4User: <p4-user-name>,
+	p4Workspace: <p4-workspace>,
+    p4Host: <p4-host>,
     p4ExePath: "C:/Program Files/Perforce/p4.exe",
     workingDirectory: "./",
 };
