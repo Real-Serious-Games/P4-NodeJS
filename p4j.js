77ae34ac03e86a2f65a1bc392e1ff26cb36229aa
Philip Warren <philip.warren@realseriousgames.com>
2016-05-30 15:31:01 +1000

    Added the add dir to change set function.
    This function will add all files in the desired path to a requested change set.

--- p4j.js
+++ p4j.js
@@ -224,6 +224,44 @@ module.exports = function (config, log) {
 				maxBuffer: execBufferSize,
 			});
 	};
+	
+	//
+	// Add dir to changeset.
+	//
+	self.addDirToChangeSet = function (changeSetId, dir, globPatern) {
+		if (!changeSetId) {
+			throw new Error('Change set id not specified.');
+		}
+		
+		if (!dir) {
+			throw new Error('Directory to add not specified.');
+		}
+		
+		globPatern = globPatern || "/**/*"
+		
+		var filesListFileName = "files.txt";
+		
+		var globOptions = {};
+		globOptions.sync = true;
+		globOptions.nodir = true;
+		
+		var os = require('os');
+		fs.writeFileSync(filesListFileName, glob(dir + globPatern, globOptions).join(os.EOL));
+			
+		var p4Args = [
+			"-u", config.p4User,
+			"-c", config.p4Workspace,
+			"-p", config.p4Host,
+			"-x", path.join(config.workingDirectory, filesListFileName),
+			"add",
+			"-c", changeSetId
+		]
+		
+		return spawn("p4", p4Args, {
+				cwd: dir,
+				maxBuffer: execBufferSize,
+			});
+	}
 
 	//
 	// Revert all checked out files.
